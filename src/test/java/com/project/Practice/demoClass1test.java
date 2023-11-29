f39c8ae22e527aec0a236e36ccc7872ae3d8e692
ashishshetty95 <ashishshetty95@gmail.com>
2023-11-29 13:38:10 +0530

    Commit 1

---
+++ src/test/java/com/project/Practice/demoClass1test.java
@@ -0,0 +1,14 @@
+package com.project.Practice;
+
+import org.testng.annotations.Test;
+
+public class demoClass1test {
+	@Test
+	public void test21() {
+		System.out.println("--test1--");
+	}
+	@Test
+	public void test22() {
+		System.out.println("--test2--");
+	}
+}
