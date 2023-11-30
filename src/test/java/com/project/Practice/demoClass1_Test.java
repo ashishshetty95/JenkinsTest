98649a962b3edb0bad3a0b5e418ee570d6ecdf10
ashishshetty95 <ashishshetty95@gmail.com>
2023-11-30 12:20:07 +0530

    Commit 8

--- src/test/java/com/project/Practice/demoClass1_Test.java
+++ src/test/java/com/project/Practice/demoClass1_Test.java
@@ -1,11 +1,29 @@
 package com.project.Practice;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class demoClass1_Test {
+	public WebDriver driver;
 	@Test(groups = "Smoke")
 	public void test21() {
+		String BROWSER=System.getProperty("browser");
+		String URL=System.getProperty("url");
+		if (BROWSER.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver();
+		}
+		else if (BROWSER.equalsIgnoreCase("edge")) {
+			driver=new EdgeDriver();
+		}
+		
+		driver.get(URL);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		System.out.println(driver.getTitle());
 		Reporter.log("--Smoke1--", true);
 	}
 	@Test(groups="Regression")
